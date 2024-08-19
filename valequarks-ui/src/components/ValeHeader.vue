e5dcf44e6b7f733aac6fb9e415b13a69fe2a3ede
Jose Cespedes <josecespedesant@gmail.com>
2024-08-18 22:31:43 -0600

    Adding first nav bar mock

---
+++ valequarks-ui/src/components/ValeHeader.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <header>
+        <nav2 class="container">
+        </nav2>
+        <nav class="container">
+            <div class="branding">
+                <ul class="nav-routes">
+                    <RouterLink to="/">Inicio</RouterLink>
+                    <RouterLink to="/donations">Donaciones</RouterLink>
+                    <RouterLink to="/services">Servicios</RouterLink>
+                    <RouterLink to="/resources">Recursos</RouterLink>
+                    <RouterLink to="/blog">Blog</RouterLink>
+                    <RouterLink to="/merch">Mercancia</RouterLink>
+                    <RouterLink to="/collaborators">Colaboradores</RouterLink>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: #272b00;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 20px 50px;
+
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      img {
+        max-width: 50px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 50px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: #f9fbca;
+      }
+    }
+  }
+  nav2 {
+    display: flex;
+    align-items: center;
+    padding: 100px;
+    background-color: #bec092;
+    min-width: 100%;
+    width: auto;
+  }
+}
+</style>
---
+++ valequarks-ui/src/router/index.js
@@ -0,0 +1,63 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/donations',
+      name: 'donations',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/services',
+      name: 'services',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/resources',
+      name: 'resources',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/merch',
+      name: 'merch',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/collaborators',
+      name: 'collaborators',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
